9db8dbdce61cc4a75d3bf7cadbb0a312371f95e3
xanf-code <darshanaswathh@gmail.com>
2022-03-24 23:59:21 +0530

    🌱

--- ui/Footer/Footer.tsx
+++ ui/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 function Footer() {
     return (
         <footer className="flex flex-col justify-center items-start max-w-2xl mx-auto w-full mb-8">
-            <hr class="w-full border-1 border-gray-200 dark:border-gray-800 mb-8"></hr>
+            <hr className="w-full border-1 border-gray-200 dark:border-gray-800 mb-8"></hr>
             <a
                 className="flex items-center justify-center gap-2"
                 href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
